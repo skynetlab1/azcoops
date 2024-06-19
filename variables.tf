889be2b3ea6f9c3acdcf2d8377d75006806b3431
Sky Soft Bulgaria <expertpcin@gmail.com>
2024-06-20 02:02:27 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-9variable "location" {
+variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
   type        = string
   default     = "westeurope"
