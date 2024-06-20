df6e2d3aa3663e411a0b5083aa6a92b55f4d782b
Ruslan Todorov <expertpcin@.com>
2024-06-20 22:39:31 +0300

    CloudOk

--- cloud.tf
+++ cloud.tf
@@ -1,14 +1,15 @@
 terraform {
-  required_providers {          azurerm = {
-      source  = "hashicorp/>
+  required_providers {                          azurerm = {
+      source  = "hashicorp/azurerm"
       version = ">=3.108.0"
-    }
+}
   }
+
 cloud {
     organization = "AzCoOps"
 
     workspaces {
-      name = "AzCoOp
+      name = "AzCoOp"
 }   
   }
 
